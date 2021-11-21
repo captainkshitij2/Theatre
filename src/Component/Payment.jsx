d8d0b2dbb1b035624ef5f93854c8c0244a24126e
kshitij <kshitij4242@gmail.com>
2021-11-21 20:24:45 +0530

    added some changes

--- src/Component/Payment.jsx
+++ src/Component/Payment.jsx
@@ -1,37 +1,49 @@
-import React from 'react';
-import '../Style/home.css'
-import {Link} from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import React from "react";
+import "../Style/home.css";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {Button } from 'react-bootstrap'
+
 
 function Payment() {
-    const state=useSelector(state=>state.dataSlice)
+  const state = useSelector((state) => state.dataSlice);
   return (
     <>
-    <header>
-      <h2>Payment Window</h2>
-    </header>
-    <main>
-        <div>
-            Seats booked are: {state.arrangement.map((item,index)=>{return<span key={index}>{item.map((item2,index2)=>{
-                if(item2[1]==true && item2[1]!=999){
-                    return item2[0]+","
-                }
-                else{
-                    return null
-                }
-            })}</span>})}
-        </div>
-        <div>
-            Total Amount:₹{state.amount}
-        </div>
+      <center>
+        <header>
+          <h1>Payment Window</h1>
+        </header>
+        <main>
+          <div>
+            <h2>
+              Seats booked are:{" "}
+              {state.arrangement.map((item, index) => {
+                return (
+                  <span key={index}>
+                    {item.map((item2, index2) => {
+                      if (item2[1] == true && item2[1] != 999) {
+                        return item2[0] + ",";
+                      } else {
+                        return null;
+                      }
+                    })}
+                  </span>
+                );
+              })}
+            </h2>
+          </div>
+          <div>
+            <h4>Total Amount:₹{state.amount}</h4>
+          </div>
 
-    <Link to="/book"><button>Go back</button></Link>
-    </main>
-    <footer>
-
-    </footer>
+          <Link to="/book">
+            <Button>Go back</Button>
+          </Link>
+        </main>
+        <footer></footer>
+      </center>
     </>
-  )
+  );
 }
 
 export default Payment;
